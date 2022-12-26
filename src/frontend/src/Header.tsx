2c2bef176e6a7916a29346d47d47408e5b13c135
Abran Lezama <lezama.abran@gmail.com>
2022-12-26 14:47:33 -0800

    implemented keycloack service for the frontend

---
+++ src/frontend/src/Header.tsx
@@ -0,0 +1,6 @@
+import {Button, Icon, Menu} from "semantic-ui-react";
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
+import {connect, ConnectedProps} from "react-redux";
+import {RootState} from "./types";
+
